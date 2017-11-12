ef29d23421219363b9f05113945c740f0dfb2327
rottin <rottinlew@163.com>
2017-11-12 20:16:39 +0800

    A UrlConnection demo,get'hello' from server

--- app/src/main/java/com/rottin/administrator/hellonews/ChatActivity.java
+++ app/src/main/java/com/rottin/administrator/hellonews/ChatActivity.java
@@ -1,6 +1,8 @@
 package com.rottin.administrator.hellonews;
 
 import android.content.Context;
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.CellIdentityGsm;
@@ -14,6 +16,12 @@ import android.widget.ListView;
 
 import com.github.library.bubbleview.BubbleTextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
@@ -21,9 +29,12 @@ import java.util.zip.Inflater;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private static final String TAG = "ChatActivity";
     private ListView chatListView;
     private ChatAdapter chatAdapter;
     private ArrayList<ChatData> chatArrayList = null;
+    private static final String HOST = "172.20.46.30";
+    private static final int PORT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +43,8 @@ public class ChatActivity extends AppCompatActivity {
         init();
         chatAdapter = new ChatAdapter(this, chatArrayList);
         chatListView.setAdapter(chatAdapter);
-        for (int i=0;i<20;i++){
-            try {
-                sendAChat("早上好"+i,0);
-                sendAChat("早上好"+i,1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        mAsyncTask asyncTask = new mAsyncTask();
+        asyncTask.execute();
     }
 
     //初始化
@@ -51,13 +56,14 @@ public class ChatActivity extends AppCompatActivity {
     //发送一个聊天内容（在界面添加一个气泡
     private void sendAChat(String content, int type) throws InterruptedException {
 
-        // TODO: 2017/11/12 如果同时大量添加，气泡顺序混乱！
+        // TODO: 2017/11/12 如果同时大量添加，气泡顺序混乱
 
         ChatData chatData = new ChatData(content, type);
         chatArrayList.add(chatData);
         chatAdapter.notifyDataSetChanged();
         Thread.sleep(100);
     }
+
 
     private class ChatAdapter extends BaseAdapter {
 
@@ -103,6 +109,49 @@ public class ChatActivity extends AppCompatActivity {
             Log.d("Chat", chatArrayList.get(position).getContent() + "");
             bubbleTextView.setText(contentToChar, 0, contentToChar.length);
             return convertView;
+        }
+    }
+
+    private class mAsyncTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            String urlName = "http://" + HOST + ":" + PORT + "/hello";
+            BufferedReader in = null;
+            String content = "";
+            try {
+                URL readUrl = new URL(urlName);
+                URLConnection conn = readUrl.openConnection();
+                conn.connect();
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    content += line+"\n";
+                    Log.d(TAG,"line:"+line);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(in != null)
+                        in.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return content;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                sendAChat(s, 0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
